9bec73809fe9e5149646256e019a2de30628f0c3
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-16 11:44:08 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const myForm = document.querySelector("form");
+	myForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const formInput = myForm.querySelector("#new-task-description").value;
+	submitTodo(formInput)
+	myForm.reset()
+	})
 });
+
+const submitTodo = (task) => {
+	const ul = document.querySelector("#tasks")
+	const li = document.createElement("li");
+	const btn = document.createElement("button");
+	btn.addEventListener("click", deleteList)
+	btn.innerHTML = "<strong>DELETE</strong>";
+	li.textContent =`${task} `;
+	li.appendChild(btn)
+	ul.appendChild(li);
+	
+  }
+  
+  const deleteList = (event) => {
+	event.target.parentNode.parentNode.remove();
+  }
